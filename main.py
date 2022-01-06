17dd06f19693df0ead09f9d16fb68df7f6ee41ad
MalkowiakMatthew22 <815306@mystma.org>
2022-01-06 09:38:29 -0600

    First Encounter done

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ points = points - strength
 print("Hey, you chose that, not me. STRENGTH SET TO " + str(strength) + "\n")
 print("You have " + str(points) + " skill points remaining.")
 
-dexterity = int(input("How nimble are you? This impacts your ability to move unseen, be flexible, or other deft and precise movements of the limbs and fingers. (0-100) \n>"))
+dexterity = int(input("How nimble, agile, and quick are you? This impacts your ability to move unseen, be flexible, or other deft and precise movements of the limbs and fingers. (0-100) \n>"))
 
 if dexterity > 100 or dexterity < 0 or dexterity > points:
     print("I am afraid that isn't a valid number. In a place like this, one wrong step is enough to get you killed.\nYOU LOSE!")
@@ -61,7 +61,7 @@ print("You have completed stat selection. Here are your stats: \n- STRENGTH: "+ 
 
 #Intro
 
-print("\n Your eyes dart open as you arise from mound of warm sand. Confused, you stare at your body and quickly brush yourself off as cool air breezes over you. 'Ah, I see you have awoken. It's about time,' a loud voice echos.\n You look around to find the source of the voice and find that you are in what appears to be a muggy sand-ridden cave with magically lit torch sconces lining the inside. 'If you survive and escape I will drop all charges against you for your crimes. I simply want to see if the rumors about your feats are true. Please, move forward lest you wish to stay stranded.'")
+print("\n Your eyes dart open as you arise from mound of warm sand. Confused, you stare at your body and quickly brush yourself off as cool air breezes over you. 'Ah, I see you have awoken. It's about time,' a loud voice echos.\n You look around to find the source of the voice and find that you are in what appears to be a muggy sand-ridden cave with magically lit torch sconces lining the inside.\n'If you survive and escape I will drop all charges against you for your crimes. I simply want to see if the rumors about your feats are true. Please, move forward lest you wish to stay stranded.'")
 
 #First Encounter
 print("You encounter an imposing wall with large worn stalagmites standing close to the base. The wall stands tall, cracked, and unmoving. What do you do?")
@@ -73,34 +73,44 @@ choice = input("Please, choose a number-->")
 
 if choice == "1":
     if int(strength) > 80:
-        print("--The wall shatters in awe of your divine strength. Your muscles are machine--")
+        print("--Your muscles, wrought as iron and machined to near perfection, delivers a quaking punch. The wall shatters in awe of your strength.--")
 
     else:
 
         roll = random.randrange(0, strength)
-        if roll >  20:
-            print("--The wall shatters as you laugh in the face of chance.--")
+        if roll >  30:
+            print("--The wall is sturdy and tall, but not without faults. You manage to land a strike in a fault line in the stone by shear luck. The wall shatters as you laugh in the face of chance.--")
     
         else:
-            print("--Your fist is shattered as you lean into your mighty blow. Ouch, you will need to rest that off.--\nYOU LOSE")
+            print("--Your fist is shattered as you lean into your blow. Ouch, you will need to rest that off.--\nYOU LOSE")
             exit()
 
 elif choice == "2":
     if int(dexterity) > 70:
-        print("--You find cracks in the wall and you scale the wall with ease. 'Impressive speed,' shady.--")
+        print("--You find cracks in the wall and you scale the wall with ease. 'Impressive speed,'.--")
 
     else:
 
-        roll = random.randrange(0, strength)
+        roll = random.randrange(0, dexterity)
         if roll >  20:
-            print("--.--")
+            print("--You take your time and find some spots along where the wall and the cave meet and spot some rocks which you use the climb up.--")
     
         else:
-            print("--You clammer unto and up the wall and make a small amount of headway Your footing isn't as sure as you thought it would be.--\nYOU LOSE")
+            print("--You clammer unto and up the wall and make a small amount of headway. Aaly, your footing isn't as sure as you thought it would be. You slip and are dashed upon the rocks below.--\nYOU LOSE")
             exit()
 
 elif choice == "3":
-    print("cool")
+    print("\nCool, I guess it just works. I mean, magic is so subjective and in this setting, which you don't know anything about, and just having any 'level' of intelligence is totally abstract and doesn't mean you spent your time working towards magic specifically while gaining 'intelligence'.\nBut sure, fine I guess it just works, books are boring, magic is dumb, but sure just cast 'wizard skip' or something and skip to the next trial or whatever.\nYou hear the voice speak, 'Ah, a practioner of magic yourself! Maybe they weren't lying about you. God! Wizards are so cool!'\n")
 else:
     print("I am afraid that isn't a valid number. One wrong step is enough to get you killed, and that's not any less true here.\nYOU LOSE!")
     exit()
+
+
+#Second Encounter
+
+print("After bypassing the wall you manage to stumble upon a chamber just above it. As you walk through the doorway you see that there is dense folliage covering the walls and the floor have a light moss carpet, cool and refreshing. As you look around you also notice a door set into the side of the room with the IMAGE OF A 3D CUBE ENGRAVED INTO IT. There is also a marble pedestal standing off to the side of the door with a scrambled 3x3 colored cube.")
+
+print("1. punch ")
+print("2. ")
+print("3. ")
+choice = input("Please, choose a number-->")
